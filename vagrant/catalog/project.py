d153ed17f3d5b17502185a6e6862da9dd2bbd047
Adland Lee <adland@4mfd.com>
2015-06-28 15:01:51 -0700

    feat: add list menu items

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,13 +1,31 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base
+from database_setup import Restaurant
+from database_setup import MenuItem
 
 PORT = 5000
 app = Flask(__name__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
-
 def HelloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += "%s<br />" % i.name
+        output += "%s<br />" % i.price
+        output += "%s<br />" % i.description
+        output += '<br />'
+
+    return output
 
 if __name__ == '__main__':
     app.debug = True
