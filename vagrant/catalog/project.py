64a36d88c2a270b01ea2aec4764cfd3e2bff1dd9
Adland Lee <adland@4mfd.com>
2015-06-29 14:08:15 -0700

    feat: add restaurant menu item management operations
    - add create restaurant menu item
    - add edit restaurant menu item

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import redirect
+from flask import request
 from flask import render_template
+from flask import url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base
@@ -15,34 +18,55 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/restaurants/')
+@app.route('/restaurants')
 def restaurantList():
     restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
 
 
-@app.route('/restaurants/<int:restaurant_id>/menu')
+@app.route('/restaurants/<int:restaurant_id>/menu_items')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    return render_template('restaurant_menus.html', restaurant=restaurant, items=items)
+    menuItems = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('restaurant_menus.html', restaurant=restaurant, menuItems=menuItems)
 
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
-def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+@app.route('/restaurants/<int:restaurant_id>/menu_items/create')
+def createMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('restaurant_menu_create.html', restaurant=restaurant)
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu_items', methods=['POST'])
+def storeMenuItem(restaurant_id):
+    newMenuItem = MenuItem(restaurant_id=restaurant_id,  name=request.form['name'])
+    session.add(newMenuItem)
+    session.commit()
+    return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
 
 
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
-def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+@app.route('/restaurants/<int:restaurant_id>/menu_items/<int:menuItem_id>/edit')
+def editMenuItem(restaurant_id, menuItem_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menuItem = session.query(MenuItem).filter_by(id=menuItem_id).one()
+    return render_template('restaurant_menu_edit.html', restaurant=restaurant, menuItem=menuItem)
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu_items/<int:menuItem_id>', methods=['POST'])
+def updateMenuItem(restaurant_id, menuItem_id):
+    print "request method: %s" % request.method
+    menuItem = session.query(MenuItem).filter_by(restaurant_id=restaurant_id, id=menuItem_id).one()
+    menuItem.name=request.form['input-menuItem-name']
+    session.add(menuItem)
+    session.commit()
+    return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
 
 
 # Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id, menu_id):
+@app.route('/restaurants/<int:restaurant_id>/menu_items/<int:menuItem_id>/delete')
+def deleteMenuItem(restaurant_id, menuItem_id):
     return "page to delete a menu item. Task 3 complete!"
 
 
