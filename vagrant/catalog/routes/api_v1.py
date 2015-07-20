51c057ff0e49cc50ad00bf16443b3c63aa06973a
Adland Lee <adland@4mfd.com>
2015-07-20 13:52:56 -0700

    refactor: split api routes to separate file
    for API v1 routes
    
    - update list course
    - add show course
    - add list course skills
    - add show course skill

--- vagrant/catalog/routes/api_v1.py
+++ vagrant/catalog/routes/api_v1.py
@@ -24,21 +24,49 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
 
-APPLICATION_NAME = "Academy Concepts"
-
-app = Flask(__name__)
-engine = create_engine(DatabaseEngineURL)
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
+DBSession = sessionmaker()
 session = DBSession()
 
 api_v1 = Blueprint('api_v1', __name__)
 
 
-@api_v1.route('/course/json')
-def APIv1_listCoursesJSON():
+@api_v1.route('/json/course/')
+def API_v1_listCoursesJSON():
     courses = session.query(Course).all()
     return jsonify(Courses=[course.serialize for course in courses])
 
-# @api_v1.route('/course/')
+
+@api_v1.route('/json/course/<int:course_id>')
+def API_v1_showCourseJSON(course_id):
+    try:
+        course = session.query(Course).filter_by(id=course_id).one()
+    except:
+        return abort(404)
+
+    return jsonify(Course=course.serialize)
+
+
+@api_v1.route('/json/course/<int:course_id>/skill')
+def API_v1_listCourseSkillssJSON(course_id):
+    try:
+        course = session.query(Course).filter_by(id=course_id).one()
+    except:
+        return abort(404)
+
+    skills = session.query(Skill).filter_by(course_id=course.id).all()
+    return jsonify(Skills=[skill.serialize for skill in skills])
+
+
+@api_v1.route('/json/course/<int:course_id>/skill/<int:skill_id>')
+def API_v1_showCourseSkillJSON(course_id, skill_id):
+    try:
+        course = session.query(Course).filter_by(id=course_id).one()
+    except:
+        return abort(404)
+
+    try:
+        skill = session.query(Skill).filter_by(id=skill_id).one()
+    except:
+        return abort(404)
+
+    return jsonify(Skill=skill.serialize)
