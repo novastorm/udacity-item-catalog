8b1fd3fcdca11001ae377a2725bed144295ed932
Adland Lee <adland@4mfd.com>
2015-11-19 23:03:45 -0800

    update secret_key declaration

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -45,8 +45,8 @@ def utility_processor():
         getUserId=auth.getUserId,
         getUserInformation=auth.getUserInformation)
 
+app.secret_key = 'super_secret_key'
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
   app.debug = True
   app.run(host = '0.0.0.0', port = 5000)
